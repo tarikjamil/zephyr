3358970c18a1546dd9f0095d99a69b740cc23b9f
tarikjamil <tarikjamill@gmail.com>
2023-07-31 18:07:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // split start
-
 let texthero;
 
 // Split the text up
@@ -43,57 +42,6 @@ function pageLoad() {
 }
 pageLoad();
 
-$("[animation='split-stagger']").each(function (index) {
-  let target = $(this).find("[animation='split-stagger-el']");
-
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: $(this),
-      start: "top bottom -=100",
-    },
-  });
-
-  tl.from(
-    target,
-    {
-      y: "100%",
-      ease: "Quint.easeOut",
-      duration: 1,
-      stagger: {
-        each: 0.1,
-        from: "start",
-      },
-    },
-    0
-  );
-});
-
-$("[animation='split-stagger-20']").each(function (index) {
-  let target = $(this).find("[animation='split-stagger-el']");
-
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: $(this),
-      start: "top bottom -=100",
-    },
-  });
-
-  tl.from(
-    target,
-    {
-      y: "20rem",
-      opacity: 0,
-      ease: "Quint.easeOut",
-      duration: 1,
-      stagger: {
-        each: 0.1,
-        from: "start",
-      },
-    },
-    0
-  );
-});
-
 $("[animation='fade-in']").each(function (index) {
   let target = $(this);
 
@@ -111,52 +59,6 @@ $("[animation='fade-in']").each(function (index) {
       opacity: 0,
       ease: "Quint.easeOut",
       duration: 1,
-    },
-    0
-  );
-});
-
-$("[animation='parallax']").each(function (index) {
-  let target = $(this);
-
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: $(this),
-      start: "top bottom",
-      end: "bottom top",
-      ease: "Quint.easeOut",
-      duration: 1,
-      scrub: true,
-    },
-  });
-
-  tl.from(
-    target,
-    {
-      y: "-20%",
-    },
-    0
-  );
-});
-
-$("[animation='parallax-2']").each(function (index) {
-  let target = $(this);
-
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: $("[animation='parallax--wrapper']"),
-      start: "top bottom",
-      end: "bottom top",
-      ease: "Quint.easeOut",
-      duration: 1,
-      scrub: true,
-    },
-  });
-
-  tl.from(
-    target,
-    {
-      y: "40%",
     },
     0
   );
@@ -207,30 +109,4 @@ $(".navbar--menu-trigger").click(function () {
     );
   }
   $(this).data("clicks", !clicks);
-});
-
-// accordion navbar --------------------- //
-$(".navbar-link.is-dropdown").on("click", function () {
-  // Close other accordions when opening new one
-  if (!$(this).hasClass("open")) {
-    $(".navbar-link.is-dropdown.open").click();
-  }
-  // Save the sibling of the toggle we clicked on
-  let sibling = $(this).siblings(".navbar-dropdown-list");
-  let animationDuration = 500;
-
-  if ($(this).hasClass("open")) {
-    // Close the content div if already open
-    sibling.animate({ height: "0px" }, animationDuration);
-  } else {
-    // Open the content div if already closed
-    sibling.css("height", "auto");
-    let autoHeight = sibling.height();
-    sibling.css("height", "0px");
-    sibling.animate({ height: autoHeight }, animationDuration, () => {
-      sibling.css("height", "auto");
-    });
-  }
-  // Open and close the toggle div
-  $(this).toggleClass("open");
 });
