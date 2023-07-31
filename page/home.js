54ce581aea06fb798fb342aab59736a58de181d2
tarikjamil <tarikjamill@gmail.com>
2023-07-31 18:13:14 +0100

    Update home.js

--- page/home.js
+++ page/home.js
@@ -21,3 +21,27 @@ $("[animation='hero--trigger']").each(function (index) {
     0
   );
 });
+
+$(".is--home-experience.is--first").each(function (index) {
+  let target = $(".circle--bg");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $(this),
+      start: "top bottom",
+      end: "bottom center",
+      ease: "Quint.easeOut",
+      duration: 1,
+      scrub: true,
+    },
+  });
+
+  tl.to(
+    target,
+    {
+      width: "76vw",
+      height: "76vw",
+    },
+    0
+  );
+});
