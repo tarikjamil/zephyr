09ea1c075892033e18839d89b281c8fd653a21ea
tarikjamil <tarikjamill@gmail.com>
2023-07-31 17:39:20 +0100

    edits

---
+++ page/home.js
@@ -0,0 +1,22 @@
+$("[animation='hero--trigger']").each(function (index) {
+  let target = $("[animation='parallax-hero']");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $(this),
+      start: "top top",
+      end: "bottom top",
+      ease: "Quint.easeOut",
+      duration: 1,
+      scrub: true,
+    },
+  });
+
+  tl.to(
+    target,
+    {
+      y: "-15%",
+    },
+    0
+  );
+});
