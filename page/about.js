d05c56ce9c9132985f2e25a7a09d1bbdf326183b
tarikjamil <tarikjamill@gmail.com>
2023-08-04 10:14:14 +0100

    Update about.js

---
+++ page/about.js
@@ -0,0 +1,22 @@
+$("[animation='parallax--trigger']").each(function (index) {
+  let target = $(this).find("[animation='parallax']");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $(this),
+      start: "top bottom",
+      end: "bottom top",
+      ease: "Quint.easeOut",
+      duration: 1,
+      scrub: true,
+    },
+  });
+
+  tl.to(
+    target,
+    {
+      y: "-20%",
+    },
+    0
+  );
+});
