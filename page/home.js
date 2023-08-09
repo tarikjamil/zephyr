c9beae7bbd53e10b7c84ebddbda60406981abfe3
tarikjamil <tarikjamill@gmail.com>
2023-08-09 13:20:06 +0100

    edits

--- page/home.js
+++ page/home.js
@@ -237,7 +237,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     ScrollTrigger.create({
       trigger: triggerEl,
       start: "top center",
-      end: "bottom top",
+      end: "bottom center",
       onToggle: ({ self, isActive }) => {
         if (isActive) {
           targetEl.addClass(classSetting);
